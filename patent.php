147fe2c464a709d44bfdff610bc19d80257bb4af
JaejinLee <opiximeo@gmail.com>
2016-11-14 18:34:42 +0900

    Update patent.php

--- patent.php
+++ patent.php
@@ -99,14 +99,6 @@ Header Section Start
 <section class="blankpage">
     <div class="container">
         <div class="row">
-
-
-
-
-
-
-
-
             <div class="col-md-12">
                 <div class="block" style="text-align:justify">
                     <h3 class="count">Patent :  </h3>
@@ -122,6 +114,8 @@ Header Section Start
                         <li>- 멀티 홉 언더레이 인지 라디오 네트워크를 위한 물리계층 보안 평가 시스템(An Evaluation System of Physical Layer Security for Multi-hop Underlay Cognitive Radio Networks)(Korea Patent Application Number: 10-2015-0182105, Date: 12-18-2015)</li>
                         <li>- LED를 이용한 가시광 양방향 통신 방법(Visible Light Bi-Direction Communication Method using LED)(Korea Patent Application Number: 10-2015-0144515, Date: 10-16-2015)</li>
                     </ul>
+                    <div>
+                    </div>
                     <h3 class="count">Program :  </h3>
                     <ul>
                         <li>- 모바일 애드혹 무선 센서네트워크에서 에어지 하베스팅인지 엔트로피기반 방향성 안내 라우팅 (프로그램등록번호: 제 C-2016-019275호, Date: 08-12-2016)</li>
@@ -134,44 +128,6 @@ Header Section Start
 
         </div>
     </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 <!--촋
 >>>>>>> 33d2e3ae49569daaf2fcc41958f64205dd9b64c5

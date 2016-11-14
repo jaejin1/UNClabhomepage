06f7f2539605ffecc2988b43e45babd3d96a996e
jaejin1 <opiximeo@gmail.com>
2016-11-14 17:52:39 +0900

    patent

--- patent.php
+++ patent.php
@@ -103,25 +103,23 @@ Header Section Start
                 <div class="block" style="text-align:justify">
                     <h3 class="count">Patent :  </h3>
                     <ul>
-                        <li>모바일 애드혹 네트워크에서 경로 안정성 기반 지오멀티캐스트 라우팅 방법(Geomulticast Routing Method using Route Stability in Mobile Ad-hoc Wireless Network)(Korea Patent Number:10-1480627, Date: 01-02-2015)</li>
-                        <li>가시광 통신을 이용한 IoT 모니터링 시스템(IoT Monitoring System based on Visible Light Communication)(Korea Patent Application Number: 10-2016-0150505, Date: 11-11-2016)</li>
-                        <li>모바일기기의 3D 터치를 이용한 커서 스크롤 제어방법(Cursor Scrolling Control Method using A 3D Touch of A Mobile Device)(Korea Patent Application Number: 10-2016-0057099, Date: 05-10-2016)</li>
-                        <li>VDI 환경에서 3D 터치방식을 이용한 모바일 기기의 단축키 입력장치 및 입력방법(Short Cut Input Device and Method of Mobile Terminal using 3D Touch Input Type in VDI Environments)(Korea Patent Application Number: 10-2016-0057101, Date: 05-10-2016)</li>
-                        <li>모바일 클라우드 컴퓨팅 클라이언트 환경에서 3D 터치를 이용한 기기의 마우스 입력장치 및 입력방법(Mouse Input Device and Method of Mobile Terminal using 3D Tpoch Input Type in Mobile Cloud Computing Client Environments)(Korea Patent Application Number: 10-2016-0057104, Date: 05-10-2016)</li>
-                        <li>인지 라디오 네트워크를 위한 소프트-하드 융합기반 협력 스펙트럼 센싱 방법(A Soft-Hard Combination-based Cooperative Spectrum Sensing Scheme for Cognitive Radio Networks)(Korea Patent Application Number: 10-2016-0011608, Date: 01-29-2016)</li>
-                        <li>멀티 레이트 모바일 애드혹 무선 네트워크에서 신뢰성 고속 데이터 전송을 위한 안정성 인지 협력 라우팅 방법(A Stability-aware Cooperative Routing Method for Reliable High-speed Data Transmission in Multi-rate Mobile Ad-hoc Wireless Networks)(Korea Patent Application Number: 10-2016-0004792, Date: 01-14-2016)</li>
-                        <li>하드웨어 왜곡을 고려한 물리계층보안 무선통신 시스템(Wireless Networks System of Physical Layer Security under Hardware Impairments)(Korea Patent Application Number: 10-2015-0182095, Date: 12-18-2015)</li>
-                        <li>멀티 홉 언더레이 인지 라디오 네트워크를 위한 물리계층 보안 평가 시스템(An Evaluation System of Physical Layer Security for Multi-hop Underlay Cognitive Radio Networks)(Korea Patent Application Number: 10-2015-0182105, Date: 12-18-2015)</li>
-                        <li>LED를 이용한 가시광 양방향 통신 방법(Visible Light Bi-Direction Communication Method using LED)(Korea Patent Application Number: 10-2015-0144515, Date: 10-16-2015)</li>
+                        <li>- 모바일 애드혹 네트워크에서 경로 안정성 기반 지오멀티캐스트 라우팅 방법(Geomulticast Routing Method using Route Stability in Mobile Ad-hoc Wireless Network)(Korea Patent Number:10-1480627, Date: 01-02-2015)</li>
+                        <li>- 가시광 통신을 이용한 IoT 모니터링 시스템(IoT Monitoring System based on Visible Light Communication)(Korea Patent Application Number: 10-2016-0150505, Date: 11-11-2016)</li>
+                        <li>- 모바일기기의 3D 터치를 이용한 커서 스크롤 제어방법(Cursor Scrolling Control Method using A 3D Touch of A Mobile Device)(Korea Patent Application Number: 10-2016-0057099, Date: 05-10-2016)</li>
+                        <li>- VDI 환경에서 3D 터치방식을 이용한 모바일 기기의 단축키 입력장치 및 입력방법(Short Cut Input Device and Method of Mobile Terminal using 3D Touch Input Type in VDI Environments)(Korea Patent Application Number: 10-2016-0057101, Date: 05-10-2016)</li>
+                        <li>- 모바일 클라우드 컴퓨팅 클라이언트 환경에서 3D 터치를 이용한 기기의 마우스 입력장치 및 입력방법(Mouse Input Device and Method of Mobile Terminal using 3D Tpoch Input Type in Mobile Cloud Computing Client Environments)(Korea Patent Application Number: 10-2016-0057104, Date: 05-10-2016)</li>
+                        <li>- 인지 라디오 네트워크를 위한 소프트-하드 융합기반 협력 스펙트럼 센싱 방법(A Soft-Hard Combination-based Cooperative Spectrum Sensing Scheme for Cognitive Radio Networks)(Korea Patent Application Number: 10-2016-0011608, Date: 01-29-2016)</li>
+                        <li>- 멀티 레이트 모바일 애드혹 무선 네트워크에서 신뢰성 고속 데이터 전송을 위한 안정성 인지 협력 라우팅 방법(A Stability-aware Cooperative Routing Method for Reliable High-speed Data Transmission in Multi-rate Mobile Ad-hoc Wireless Networks)(Korea Patent Application Number: 10-2016-0004792, Date: 01-14-2016)</li>
+                        <li>- 하드웨어 왜곡을 고려한 물리계층보안 무선통신 시스템(Wireless Networks System of Physical Layer Security under Hardware Impairments)(Korea Patent Application Number: 10-2015-0182095, Date: 12-18-2015)</li>
+                        <li>- 멀티 홉 언더레이 인지 라디오 네트워크를 위한 물리계층 보안 평가 시스템(An Evaluation System of Physical Layer Security for Multi-hop Underlay Cognitive Radio Networks)(Korea Patent Application Number: 10-2015-0182105, Date: 12-18-2015)</li>
+                        <li>- LED를 이용한 가시광 양방향 통신 방법(Visible Light Bi-Direction Communication Method using LED)(Korea Patent Application Number: 10-2015-0144515, Date: 10-16-2015)</li>
                     </ul>
                 </div>
             </div>
 
         </div>
     </div>
-</section>
 
-<section class="blankpage">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -138,6 +136,7 @@ Header Section Start
         </div>
     </div>
 </section>
+
 <!--촋
 ==================================================
 Footer Section Start

9def5128326ce588137fd93341ed8bc651496ba7
jaejin1 <opiximeo@gmail.com>
2018-02-05 17:13:00 +0900

    patent update

--- patent.php
+++ patent.php
@@ -103,12 +103,10 @@ Header Section Start
                 <div class="block" style="text-align:justify">
                     <h3 class="count">Patent :  </h3>
                     <ul>
-                        <li>- Multi to One 가시광통신을 이용한 IoT 시스템(IoT System using Multi-to-One Visible Light Communication) ((Korea Patent Number: 10-2017-0163374, Date: 2017년 11월 30일)</li>
-                        <li>- 모바일 애드혹 네트워크에서 경로 안정성 기반 지오멀티캐스트 라우팅 방법(Geomulticast Routing Method using Route Stability in Mobile Ad-hoc Wireless Network)(Korea Patent Number: 10-1480627, Date: 2015년 01월 02일)</li>
-                        <li>- 하드웨어 왜곡을 고려한 물리계층보안 무선통신시스템(Wireless Networks System of Physical Layer Security under Hardware Impairments) (Korea Patent Number: 10-1726085, Date: 2017년 04월 05일)</li>
-                        <li>- 멀티 레이트 모바일 애드혹 무선 네트워크에서 신뢰성 고속 데이터 전송을 위한 안정성 인지 협력 라우팅 방법(A Stability-aware Cooperative Routing Method for Reliable High-speed Data Transmission in Multi-rate Mobile Ad-hoc Wireless Networks)(Korea Patent Application Number: 10-1755979, Date: 2017년 07월 03일)</li>
+                        <li>- 멀티 레이트 모바일 애드혹 무선 네트워크에서 신뢰성 고속 데이터 전송을 위한 안정성 인지 협력 라우팅 방법(A Stability-aware Cooperative Routing Method for Reliable High-speed Data Transmission in Multi-rate Mobile Ad-hoc Wireless Networks)(Korea Patent Number: 10-1755979, Date: 2017년 07월 03일)</li>
                         <li>- 하드웨어 왜곡을 고려한 물리계층보안 무선통신시스템(Wireless Networks System of Physical Layer Security under Hardware Impairments) (Korea Patent Number: 10-1726085, Date: 2017년 04월 05일)</li>
                         <li>- 모바일 애드혹 네트워크에서 경로 안정성 기반 지오멀티캐스트 라우팅 방법(Geomulticast Routing Method using Route Stability in Mobile Ad-hoc Wireless Network)(Korea Patent Number: 10-1480627, Date: 2015년 01월 02일)</li>
+                        <li>- Multi to One 가시광통신을 이용한 IoT 시스템(IoT System using Multi-to-One Visible Light Communication) (Korea Patent Application Number: 10-2017-0163374, Date: 2017년 11월 30일)</li>
                         <li>- 경로 단절 및 손실 검증 기능을 갖는 가시광 무선통신 시스템(Visible Light Communication System Having Pass Lose verification Function) (Korea Patent Application Number: 10-2017-0049779, Date: 2017년 04월 18일)</li>
                         <li>- IoT 기반의 모바일 스마트 태양광 발전 관리 시스템(IoT based Mobile Smart Solar Power Generation Management)(Korea Patent Application Number: 10-2017-0051844, Date: 2017년 04월 21일)</li>
                         <li>- 가시광 통신을 이용한 IoT 모니터링 시스템(IoT Monitoring System based on Visible Light Communication)(Korea Patent Application Number: 10-2016-0150505, Date: 2016년 11월 11일)</li>
@@ -116,7 +114,6 @@ Header Section Start
                         <li>- VDI 환경에서 3D 터치방식을 이용한 모바일 기기의 단축키 입력장치 및 입력방법(Short Cut Input Device and Method of Mobile Terminal using 3D Touch Input Type in VDI Environments)(Korea Patent Application Number: 10-2016-0057101, Date: 2016년 05월 10일)</li>
                         <li>- 모바일 클라우드 컴퓨팅 클라이언트 환경에서 3D 터치를 이용한 기기의 마우스 입력장치 및 입력방법(Mouse Input Device and Method of Mobile Terminal using 3D Tpoch Input Type in Mobile Cloud Computing Client Environments)(Korea Patent Application Number: 10-2016-0057104, Date: 2016년 05월 10일)</li>
                         <li>- 인지 라디오 네트워크를 위한 소프트-하드 융합기반 협력 스펙트럼 센싱 방법(A Soft-Hard Combination-based Cooperative Spectrum Sensing Scheme for Cognitive Radio Networks)(Korea Patent Application Number: 10-2016-0011608, Date: 2016년 01월 29일)</li>
-                        <li>- 멀티 레이트 모바일 애드혹 무선 네트워크에서 신뢰성 고속 데이터 전송을 위한 안정성 인지 협력 라우팅 방법(A Stability-aware Cooperative Routing Method for Reliable High-speed Data Transmission in Multi-rate Mobile Ad-hoc Wireless Networks)(Korea Patent Application Number: 10-2016-0004792, Date: 2016년 01월 14일)</li>
                         <li>- 멀티 홉 언더레이 인지 라디오 네트워크를 위한 물리계층 보안 평가 시스템(An Evaluation System of Physical Layer Security for Multi-hop Underlay Cognitive Radio Networks)(Korea Patent Application Number: 10-2015-0182105, Date: 2015년 12월 18일)</li>
                         <li>- LED를 이용한 가시광 양방향 통신 방법(Visible Light Bi-Direction Communication Method using LED)(Korea Patent Application Number: 10-2015-0144515, Date: 2015년 10월 16일)</li>
                     </ul>
@@ -125,6 +122,9 @@ Header Section Start
                     </div>
                     <h3 class="count">Program :  </h3>
                     <ul>
+                        <li>- 사물인터넷(IoT) 기반 태양광 발전 구조물 안전진단 시스템(영농형)((프로그램등록번호: 제 C-2018-002532호, Date: 2018년 01월 29일)</li>
+                        <li>- 사물인터넷(IoT) 기반 태양광 발전 구조물 안전진단 시스템(수상형)((프로그램등록번호: 제 C-2018-002531호, Date: 2018년 01월 29일)</li>
+                        <li>- 사물인터넷(IoT) 기반 태양광 발전 구조물 안전진단 시스템(기본형)((프로그램등록번호: 제 C-2018-002530호, Date: 2018년 01월 29일)</li>
                         <li>- 스마트폰 카메리를 이용한 multi to one(다대일) 가시광통신 시스템(프로그램등록번호: 제 C-2017-033615호, Date: 2017년 11월 30일)</li>
                         <li>- RS485 통신을 이용한 M2M 기반 태양광 발전 관제 시스템((프로그램등록번호: 제 C-2017-006153호, Date: 2017년 02월 21일)</li>
                         <li>- M2M 기반 태양광 발전 관제 시스템((프로그램등록번호: 제 C-2017-006152호, Date: 2017년 02월 21일)</li>
